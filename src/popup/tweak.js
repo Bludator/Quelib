b9c8a28b4b259b3d5d0530855066d44f0daaecaf
Bludator <bludator@gmail.com>
2018-11-11 20:35:40 +0100

    Get tweak.js working

--- src/popup/tweak.js
+++ src/popup/tweak.js
@@ -1,32 +1,49 @@
-export default function tweak(instance){
-    let un = typeof instance === "function" ?instance.prototype:instance.constructor.prototype
-    let p = new Proxy(un,{
-  get: function(target, property) {
-      console.log(target,property)
-      if (target[property]!==undefined) return target[property];
-      if (!target.length) return target[0][property];
-      let properties = [];
-      for (const element of target) {
-          properties.push(element[property]);
-      }
-      return properties;
-  },
-  set: function(target, property, value) {
-      console.log(target)
-      Array.prototype.forEach.call(target, e=>e[property]=value)
-  }/*,
-  apply:function (target, thisArg, argumentsList) {
-      target.
-  }*/
-})
-typeof instance === "function" ?instance.prototype = p:instance.constructor.prototype=p
-//un.set("prototype", p).set("fn", p).set("prototype", p);
+var prototype;
+var RealPrefix;
+var ProxyPrefix;
+export default function tweak(instance, prefix = "$", p2="_") {
+    prototype = Object.getPrototypeOf(instance);
+    let p = new Proxy(Object.getPrototypeOf(prototype), handler);
+    Object.setPrototypeOf(prototype, p);
 }
 
-function set(property, value){
-    if(this[property]) this[property]=value
-    return this
+export function wrap(object) {
+    return new Proxy(object, handler);
 }
-function morph(object, into) {
-    
+
+let handler = {
+    get: function(target, property, receiver) {
+        if (target[property] !== undefined) return target[property];
+        //if (property === Symbol.iterator) return;//??
+        if (isArrayLike(receiver) && receiver[0][property]) {
+            console.log("get", receiver, property);
+            let properties = [];
+            for (let i = 0; i < receiver.length; i++) {
+                properties.push(receiver[i][property]);
+            }
+            return properties;
+        } else {
+            return undefined;
+        }
+        //if function return
+    },
+    set: function(target, property, value, receiver) {
+        if (isArrayLike(receiver) && receiver[0][property]) {
+            console.log("set", receiver, property, value);
+            for (let i = 0; i < receiver.length; i++) {
+                receiver[i][property] = value;
+            }
+        } else {
+            Object.defineProperty(receiver, property, { value });
+        }
+        return true;
+    } /*,
+    apply:function (target, thisArg, argumentsList) {
+    }*/
+};
+function isArrayLike(object) {
+    if (object instanceof Array) return true;
+    //if (object.hasOwnProperty(Symbol.iterator)) return true;//good?
+    if (object.hasOwnProperty("0") && object.hasOwnProperty("length")) return true;
+    return false;
 }
