8ac91e39c2434a27c10fc963be087433dccc4cd6
Bludator <bludator@gmail.com>
2018-11-30 21:51:42 +0100

    ++ hide/show/toggle

--- src/main.js
+++ src/main.js
@@ -30,23 +30,44 @@ queryObject.addAlias("off", "removeEventListener");
 
 /**
  * Creates and populates array in Magic proxy
- * @param {*} query 
+ * @param {*} query
  */
 function CreateObject(query) {
     let nodeList = document.querySelectorAll(query);
 
     //Object.defineProperty skips Proxy
-    Object.defineProperty(this, "length", { value: nodeList.length});
+    Object.defineProperty(this, "length", { value: nodeList.length });
     for (var i = 0; i < this.length; i++) {
-        Object.defineProperty(this, i, { value: nodeList[i]});
+        Object.defineProperty(this, i, { value: nodeList[i] });
     }
 }
+
+//Register class for hide/show/toggle
+const collapsedClass = `.QuelibCollapsed`;
+document.styleSheets[0].insertRule(
+    collapsedClass + " { display: none !important}"
+);
 
 /**
  * Constructor for query object (prototype for all $(...))
  */
 function QueryObject() {
-    //hide/show
+    this.hide = function() {
+        this.classList.add(collapsedClass);
+    };
+    this.show = function() {
+        this.classList.remove(collapsedClass);
+    };
+    this.toggle = function() {
+        for (const element of this) {
+            if (element.classList.contains(collapsedClass)) {
+                element.classList.remove(collapsedClass);
+            } else {
+                element.classList.add(collapsedClass);
+            }
+        }
+    };
+
     this.addAlias = function(alias, property) {
         Object.defineProperty(this, alias, {
             get: function() {
